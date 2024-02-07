3d5294637fb872a42544dfeeebeee1fecbdf1234
Namling <157800039+Namling9@users.noreply.github.com>
2024-02-07 09:07:38 +0000

    advance operator

---
+++ Intermediate/advoperator.js
@@ -0,0 +1,21 @@
+ // Nullish Coalescing Operator
+
+ let a = null ?? 10;
+ let b = 5 ?? 10;
+
+ console.log(a); // 10
+
+console.log(b); // 5
+
+
+// Terniary
+
+// Condition ? true : false
+
+let height = 6;
+
+height>=5.10 ? console.log(" He is eligible for modeling") : console.log("He is not eligible for modeling");
+
+// He is eligible for modeling
+
+
